92c4f19bdeeedb74129a29e32f6fa3ec21dfd2d5
Matheus Vogel de Faria <mulls4852@gmail.com>
2025-04-21 17:44:45 -0300

    chore: Adding primevue

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 import App from './App.vue'
 
-const app = createApp(App)
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
 app.use(createPinia())
 
