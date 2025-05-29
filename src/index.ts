004354981cd7eaedfe493bb39ba39f8af45804e1
Matheus Vogel de Faria <mulls4852@gmail.com>
2025-05-29 18:01:11 -0300

    chore: New build 0.1.4

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import type { App } from 'vue'
 import './styles/index.scss'
 
 import AButton from './components/AButton/AButton.vue'
@@ -10,6 +11,23 @@ import AModal from './components/AModal/AModal.vue'
 import ANavbar from './components/ANavbar/ANavbar.vue'
 import ANotification from './components/ANotification/ANotification.vue'
 import ARegister from './components/ARegister/ARegister.vue'
+
+const AureaUI = {
+  install(app: App) {
+    app.component('AButton', AButton)
+    app.component('ACard', ACard)
+    app.component('AInput', AInput)
+    app.component('AInputPassword', AInputPassword)
+    app.component('ALoading', ALoading)
+    app.component('ALogin', ALogin)
+    app.component('AModal', AModal)
+    app.component('ANavbar', ANavbar)
+    app.component('ANotification', ANotification)
+    app.component('ARegister', ARegister)
+  },
+}
+
+export default AureaUI
 
 export {
   AButton,
